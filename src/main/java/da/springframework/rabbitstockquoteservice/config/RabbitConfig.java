edaac7aa1ce22781a6fd0ce35def09ef4682ead1
christian.tavera <christian.tavera@grupoargos.com.mx>
2022-08-17 17:30:31 -0500

    81. Message Sender Configuration. Closes #2

---
+++ src/main/java/da/springframework/rabbitstockquoteservice/config/RabbitConfig.java
@@ -0,0 +1,36 @@
+package da.springframework.rabbitstockquoteservice.config;
+
+import com.rabbitmq.client.Connection;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+import reactor.rabbitmq.RabbitFlux;
+import reactor.rabbitmq.Sender;
+import reactor.rabbitmq.SenderOptions;
+
+import javax.annotation.PreDestroy;
+
+@Configuration
+public class RabbitConfig {
+
+    public static final String QUEUE = "quotes";
+
+    Mono<Connection> connectionMono;
+
+    @Bean
+    Mono<Connection> connectionMono(CachingConnectionFactory cachingConnectionFactory){
+        return Mono.fromCallable(() -> cachingConnectionFactory.getRabbitConnectionFactory().newConnection());
+    }
+
+    @PreDestroy // Before the shutting down, close de connection factory
+    public void close() throws Exception {
+        connectionMono.block().close();
+    }
+
+    @Bean
+    Sender sender(Mono<Connection> mono) {
+        // Sender use the cache connection for efficiency
+        return RabbitFlux.createSender(new SenderOptions().connectionMono(mono));
+    }
+}
