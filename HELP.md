c58cbb613156cefaf74e94a63839b08919741cd5
christian.tavera <christian.tavera@grupoargos.com.mx>
2022-08-17 17:10:39 -0500

    79. Initial Commit.

---
+++ HELP.md
@@ -0,0 +1,8 @@
+# RabbitMQ Stock Quote Service
+
+This repository has examples of Reactive Programming
+
+#### Useful Commands:
+Run RabbitMQ in a docker container and expose port 5672
+
+```docker run -d -p 5672:5672 --name rabbitmq-server rabbitmq```
---
+++ src/test/java/da/springframework/rabbitstockquoteservice/RabbitStockQuoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package da.springframework.rabbitstockquoteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitStockQuoteServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
